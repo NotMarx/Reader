236acbfe64af0c69147ddbbf5e944da0d4f98ca0
Reinhardt <notmarx.tech@gmail.com>
2021-09-24 22:00:51 +0700

    Improve stats command

--- Source/Commands/General/stats.ts
+++ Source/Commands/General/stats.ts
@@ -1,7 +1,9 @@
 "use strict"
 
 import { Command } from "../../Interfaces";
-import { EmbedOptions } from "eris";
+import { EmbedOptions, VERSION } from "eris";
+import Util from "../../Extensions/util";
+import os from "os-utils";
 
 export const command: Command = {
     name: "stats",
@@ -9,15 +11,54 @@ export const command: Command = {
     aliases: ["status"],
     category: "General",
     run: async (client, message, args) => {
-        const embed: EmbedOptions = {
-            title: `${client.user.username}'s Stats`,
-            color: client.config.COLOUR,
-            footer: {
-                text: `Made by reinhardt`,
-                icon_url: "https://cdn.discordapp.com/avatars/516186529547288576/0bf219f20a58380d381649dcde4d38e5.png?size=512"
-            }
-        }
+        const memory: number = process.memoryUsage().rss
+        const totalMemory: string = `${Util.bytes(memory).value}${Util.bytes(memory).unit} / ${Util.bytes(require("os").totalmem()).value}${Util.bytes(require("os").totalmem()).unit}`;
+        const used: number = process.memoryUsage().rss / 12800 / 12800;
 
-        message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id }});
+        os.cpuUsage(async (cpu) => {
+            const embed: EmbedOptions = {
+                title: `${client.user.username}'s Stats`,
+                description: `This is the current stats for **${client.user.username}**. \n\n Shard ID she's currently in: **${message.member.guild.shard.id}**`,
+                color: client.config.COLOUR,
+                fields: [
+                    {
+                        name: "Memory Usage",
+                        value: `${totalMemory} \n (${Math.round(used * 100) / 100}%)`,
+                        inline: true
+                    },
+                    {
+                        name: "CPU Usage",
+                        value: `${Util.round(cpu, 2)}%`,
+                        inline: true
+                    },
+                    {
+                        name: "Database Size",
+                        value: `${Util.bytes(client.database.size()).value}${Util.bytes(client.database.size()).unit}`,
+                        inline: true
+                    },
+                    {
+                        name: "NodeJS",
+                        value: "14.16.0",
+                        inline: true
+                    },
+                    {
+                        name: "Eris",
+                        value: VERSION,
+                        inline: true
+                    },
+                    {
+                        name: "Platform",
+                        value: `${process.platform.charAt(0).toUpperCase() + process.platform.slice(1)}`,
+                        inline: true
+                    }
+                ],
+                footer: {
+                    text: `Made by reinhardt`,
+                    icon_url: "https://cdn.discordapp.com/avatars/516186529547288576/0bf219f20a58380d381649dcde4d38e5.png?size=512"
+                }
+            }
+    
+            message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id }}); 
+        });
     }
 }
