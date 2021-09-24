887380a6141a1ef38089826cb4c53d880a09a3ec
Reinhardt <contact@reinhello.xyz>
2021-09-24 22:00:44 +0700

    Add utility functions

---
+++ Source/Extensions/util.ts
@@ -0,0 +1,14 @@
+"use strict";
+
+import byteSize from "byte-size";
+
+export default class Util {
+    static bytes(size: number): any {
+        return byteSize(size);
+    }
+
+    static round(value: number, precision: number): number {
+        const multi = Math.pow(10, precision || 0);
+        return Math.round(value * multi) / multi;
+    }
+}
