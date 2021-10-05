b2efbf701d04e8ce2662942f87da4d8b49b335f0
Reinhardt <notmarx.tech@gmail.com>
2021-10-05 19:12:16 +0700

    Bugfix bookmark
    Fix a bug where 'length' is undefined

--- Source/Commands/Main/bookmark.ts
+++ Source/Commands/Main/bookmark.ts
@@ -13,13 +13,23 @@ export const command: Command = {
         const prefix: string = await client.database.fetch(`Database.${message.guildID}.Prefix`) || client.config.PREFIX;
         const bookmarked: string[] = await client.database.fetch(`Database.${message.author.id}.Bookmark`);
 
+        if (!bookmarked || bookmarked.length === 0) {
+            const embed: RichEmbed = new RichEmbed()
+                .setTitle(`${message.author.username}'s Bookmarks`)
+                .setColor(client.config.COLOUR)
+                .setDescription(`Welcome! Please have a look. \n\n **Wanna remove your bookmarked Doujin?** \n Simply visit the page (\`${prefix}read <doujin_code>\`) and click the **Bookmark** button again to remove.`)
+                .addField("Bookmarked Doujin", "`None`");
+
+            return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
+        }
+
         const embed: RichEmbed = new RichEmbed()
             .setTitle(`${message.author.username}'s Bookmarks`)
             .setColor(client.config.COLOUR)
             .setDescription(`Welcome! Please have a look. \n\n **Wanna remove your bookmarked Doujin?** \n Simply visit the page (\`${prefix}read <doujin_code>\`) and click the **Bookmark** button again to remove.`)
-            .addField("Bookmarked Doujin", `\`${bookmarked.length >= 1 ? bookmarked.join("`, `") : "None"}\``);
+            .addField("Bookmarked Doujin", `\`${bookmarked.join("`, `")}\``);
 
-        return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id }});
+        return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
     }
 
 }
