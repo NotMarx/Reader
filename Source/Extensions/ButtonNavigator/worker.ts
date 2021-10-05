04707fab4ef721d17140495d67aa687489c7b43c
Reinhardt <notmarx.tech@gmail.com>
2021-10-05 08:45:55 +0700

    Bugfix button navigator

--- Source/Extensions/ButtonNavigator/worker.ts
+++ Source/Extensions/ButtonNavigator/worker.ts
@@ -1,13 +1,102 @@
 "use strict";
 
+import { AdvancedMessageContent, ComponentInteraction, EmbedOptions, Message, TextableChannel } from "eris";
+
+class ButtonNavigator {
+    embeds: EmbedOptions[];
+    embed: number;
+    invoker: Message<TextableChannel>;
+    message: Message<TextableChannel>;
+    constructor(message: Message<TextableChannel>, embeds: EmbedOptions[]) {
+        this.embeds = embeds;
+        this.embed = 1;
+        this.invoker = message;
+    }
+
+    async init() {
+        const messageContent: AdvancedMessageContent = {
+            content: `Page **${this.embed}** / **${this.embeds.length}**`,
+            embeds: [this.embeds[this.embed - 1]],
+            components: [
+                {
+                    type: 1,
+                    components: [
+                        { style: 1, type: 2, custom_id: `previous_page_${this.invoker.id}`, label: "Back" },
+                        { style: 4, type: 2, custom_id: `destroy_page`, label: "Stop" },
+                        { style: 1, type: 2, custom_id: `next_page_${this.invoker.id}`, label: "Next" },
+                    ]
+                }
+            ]
+        }
+
+        if (this.invoker.author.id === this.invoker.channel.client.user.id) {
+            this.message = await this.invoker.edit(messageContent);
+        } else {
+            this.message = await this.invoker.channel.createMessage(messageContent);
+        }
+    }
+
+    update() {
+        this.message.edit({
+            content: `Page **${this.embed}** / **${this.embeds.length}**`,
+            embed: this.embeds[this.embed - 1],
+            components: [
+                {
+                    type: 1,
+                    components: [
+                        { style: 1, type: 2, custom_id: `previous_page_${this.invoker.id}`, label: "Back" },
+                        { style: 4, type: 2, custom_id: `destroy_page`, label: "Stop" },
+                        { style: 1, type: 2, custom_id: `next_page_${this.invoker.id}`, label: "Next" },
+                    ]
+                }
+            ]
+        });
+    }
+
+    run() {
+        this.message.channel.client.on("interactionCreate", async (interaction: ComponentInteraction<TextableChannel>) => {
+                switch (interaction.data.custom_id) {
+                    case `next_page_${this.invoker.id}`:
+                        interaction.acknowledge();
+
+                        if (this.embed < this.embeds.length) {
+                            this.embed++
+                            this.update();
+                        }
+                        break;
+                    case `previous_page_${this.invoker.id}`:
+                        interaction.acknowledge();
+    
+                        if (this.embed > 1) {
+                            this.embed--;
+                            this.update();
+                        }
+                        break;
+                    case `destroy_page`:
+                        interaction.acknowledge();
+                        this.message.delete();
+                        break;
+                }
+        });
+    }
+}
+
+export default async function createPaginationEmbed(message: Message<TextableChannel>, embeds: EmbedOptions[]) {
+    const paginationEmbed = new ButtonNavigator(message, embeds);
+    await paginationEmbed.init();
+    paginationEmbed.run();
+
+    return Promise.resolve(paginationEmbed.message);
+}
+
+/* "use strict";
+
 import { ComponentInteraction, EmbedOptions, TextableChannel, InteractionButton } from "eris";
 import Reader from "../client";
 
 export default async function ButtonNavigator(client: Reader, interaction: ComponentInteraction<TextableChannel>, embeds: EmbedOptions[], buttons: InteractionButton[]): Promise<void> {
 
-    client.createInteractionResponse(interaction.id, interaction.token, {
-        type: 7,
-        data: {
+    interaction.editParent({
             content: `Page 1 / ${embeds.length}`,
             components: [
                 {
@@ -16,14 +105,14 @@ export default async function ButtonNavigator(client: Reader, interaction: Compo
                 }
             ],
             embeds: [embeds[0]]
-        }
-    });
+        });
 
     client.on("interactionCreate", async (interaction: ComponentInteraction<TextableChannel>) => {
         if (interaction.type === 3 && interaction.data.component_type === 2) {
-            const fetchMsg = await client.getMessage(interaction.channel.id, interaction.message.id);
 
-            let i: number = (fetchMsg.content.split("Page")[1].split("/")[0] as any) - 1;
+            console.log(interaction.message);
+
+            let i: number = await (interaction.message.content.split("Page")[1].split("/")[0] as any) - 1 // (fetchMsg.content.split("Page")[1].split("/")[0] as any) - 1;
 
             switch (interaction.data.custom_id) {
                 case "previousPage":
@@ -33,10 +122,9 @@ export default async function ButtonNavigator(client: Reader, interaction: Compo
                     i + 1 < embeds.length ? ++i : 0;
                     break;
                 case "stopPage":
-                    await client.database.delete(`Database.${interaction.guildID}.${interaction.member.id}.Book`);
-                    await client.deleteMessage(interaction.channel.id, interaction.message.id).catch(() => { });
-                    break;
-                default:
+                    client.database.delete(`Database.${interaction.guildID}.${interaction.member.id}.BookEmbed`);
+                    client.database.delete(`Database.${interaction.guildID}.${interaction.member.id}.Book`);
+                    client.deleteMessage(interaction.channel.id, interaction.message.id).catch(() => { });
                     break;
             }
 
@@ -55,4 +143,5 @@ export default async function ButtonNavigator(client: Reader, interaction: Compo
             });
         }
     });
-}
+} */
+ 
