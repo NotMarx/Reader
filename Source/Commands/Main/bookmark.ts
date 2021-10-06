17ba344fc76ad063f8cf64b91f473c03720f22d9
Reinhardt <notmarx.tech@gmail.com>
2021-10-06 16:33:39 +0700

    Improve bookmark command
    It'll now shows the title as well!

--- Source/Commands/Main/bookmark.ts
+++ Source/Commands/Main/bookmark.ts
@@ -1,5 +1,6 @@
 "use strict";
 
+import { API } from "nhentai-api";
 import { Command } from "../../Interfaces";
 import RichEmbed from "../../Extensions/embed";
 
@@ -10,6 +11,7 @@ export const command: Command = {
     category: "Main",
     nsfwOnly: true,
     run: async (client, message, args) => {
+        const api = new API();
         const prefix: string = await client.database.fetch(`Database.${message.guildID}.Prefix`) || client.config.PREFIX;
         const bookmarked: string[] = await client.database.fetch(`Database.${message.author.id}.Bookmark`);
 
@@ -23,11 +25,18 @@ export const command: Command = {
             return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
         }
 
+        const bookmarkedTitle: string[] = [];
+
+        for (let i = 0; i < bookmarked.length; i++) {
+            const theAPI = await api.getBook(parseInt(bookmarked[i])).then((res) => `\`[${res.id}]\` - \`${res.title.pretty}\``);
+            bookmarkedTitle.push(theAPI);
+        }
+
         const embed: RichEmbed = new RichEmbed()
             .setTitle(`${message.author.username}'s Bookmarks`)
             .setColor(client.config.COLOUR)
             .setDescription(`Welcome! Please have a look. \n\n **Wanna remove your bookmarked Doujin?** \n Simply visit the page (\`${prefix}read <doujin_code>\`) and click the **Bookmark** button again to remove.`)
-            .addField(`⭐ Bookmarked Doujin [${bookmarked.length}]`, `\`${bookmarked.join("`, `")}\``);
+            .addField(`⭐ Bookmarked Doujin [${bookmarked.length}]`, bookmarkedTitle.join("\n"));
 
         return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
     }
