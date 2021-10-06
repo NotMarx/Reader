64dbe8d7fa114a8067a3b335c5fdef290e02ab3c
Reinhardt <notmarx.tech@gmail.com>
2021-10-06 17:51:36 +0700

    Add search command
    Todo:
    - Button page navigate

---
+++ Source/Commands/Main/search.ts
@@ -0,0 +1,44 @@
+"use strict";
+
+import { API } from "nhentai-api";
+import { Command } from "../../Interfaces";
+import RichEmbed from "../../Extensions/embed";
+
+export const command: Command = {
+    name: "search",
+    description: "Search a Doujin",
+    aliases: [],
+    category: "Main",
+    nsfwOnly: true,
+    run: async (client, message, args) => {
+        const api = new API();
+
+        api.search(encodeURIComponent(args.slice(1).join(" ")), parseInt(args[0])).then((res) => {
+            if (res.books.length === 0) {
+                const embed = new RichEmbed()
+                    .setDescription("No Results Found!")
+                    .setColor(client.config.COLOUR);
+
+                return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
+            }
+
+            if (parseInt(args[0]) > res.pages) {
+                const embed = new RichEmbed()
+                    .setDescription(`Only **${res.pages}** result ${res.pages >= 2 ? "pages" : "page"} ${res.pages >= 2 ? "are" : "is"} found!`)
+                    .setColor(client.config.COLOUR);
+
+                return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
+            }
+
+            const title = res.books.map((book) => `\`[${book.id}]\` - \`${book.title.pretty}\``);
+
+            const embed = new RichEmbed()
+                .setTitle(`Page ${res.page} / ${res.pages}`)
+                .setDescription(`**Titles** \n\u2063 ${title.join("\n")}`)
+                .setColor(client.config.COLOUR);
+
+            message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
+        });
+
+    }
+}
