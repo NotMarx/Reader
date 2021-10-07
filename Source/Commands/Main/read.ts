74a45f0963bed207739c2aedee19f0a911e4de10
Reinhardt <contact@reinhello.xyz>
2021-10-07 07:00:05 +0700

    Allow multiple reading
    Including with Bugfixes from previous implementation

--- Source/Commands/Main/read.ts
+++ Source/Commands/Main/read.ts
@@ -1,8 +1,9 @@
 "use strict";
 
 import { Command } from "../../Interfaces";
-import { ActionRow, EmbedOptions } from "eris";
+import { ActionRow, EmbedOptions, Message, TextableChannel } from "eris";
 import { API } from "nhentai-api";
+import { createPaginationEmbed } from "../../Extensions/ButtonNavigator/worker";
 import RichEmbed from "../../Extensions/embed";
 import moment from "moment";
 
@@ -35,11 +36,11 @@ export const command: Command = {
             return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id }});
         }
 
-        api.getBook(parseInt(args[0])).then((res) => {
+        api.getBook(parseInt(args[0])).then(async (res) => {
             const contentTags: string[] = res.tags.filter((tag) => tag.url.startsWith("/tag")).map((tag) => `${tag.name} (${tag.count.toLocaleString()})`);
             const languageTags: string[] = res.tags.filter((tag) => tag.url.startsWith("/language")).map((tag) => tag.name);
             const uploadedAt: string = `\`${moment(res.uploaded).format("On dddd, MMMM Do, YYYY h:mm A")}\``;
-            
+
             const embed = new RichEmbed()
                 .setAuthor(args[0], `https://nhentai.net/g/${args[0]}`, "https://cdn.discordapp.com/attachments/755253854819582114/894895960931590174/845298862184726538.png")
                 .setColor(client.config.COLOUR)
@@ -56,7 +57,7 @@ export const command: Command = {
                     components: [
                         {
                             label: "Read",
-                            custom_id: "read_prop",
+                            custom_id: `read_prop_${message.id}`,
                             style: 1,
                             type: 2
                         },
@@ -75,8 +76,8 @@ export const command: Command = {
                     ]
                 }
 
-            client.database.set(`Database.${message.guildID}.${message.author.id}.Book`, args[0]);
-            message.channel.createMessage({ embeds: [embed], components: [component], messageReference: { messageID: message.id }});
+            const msg = await message.channel.createMessage({ embeds: [embed], components: [component], messageReference: { messageID: message.id }});
+            createPaginationEmbed(client, res, msg, message);
         }).catch((err) => {
             return message.channel.createMessage({ content: err, messageReference: { messageID: message.id }});
         });
