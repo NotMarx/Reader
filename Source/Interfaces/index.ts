3fc67968278e3867b3f7b617959a48c16b5d26d3
Reinhardt <notmarx.tech@gmail.com>
2021-10-14 18:07:57 +0700

    Update language for 'config' command

--- Source/Interfaces/index.ts
+++ Source/Interfaces/index.ts
@@ -1,6 +1,17 @@
 "use strict";
 
 export interface LanguageOptions {
+    ADMIN: {
+        CONFIG: {
+            NO_PERMS: string;
+            NO_OPT: string;
+            NO_PREFIX: string;
+            PREFIX_SUCCESS: string;
+            NO_LANG: string;
+            INVALID_LANG: string;
+            LANG_SUCCESS: string
+        }
+    }
     GENERAL: {
         PING: {
             TEXT: string;
