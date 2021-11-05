b320a53a705e7c9b0d7ff9bc093ad3019b75e4f6
Reinhardt <contact@reinhello.xyz>
2021-11-05 22:58:01 +0700

    Add config: language

--- Source/Commands/Admin/config.ts
+++ Source/Commands/Admin/config.ts
@@ -14,12 +14,12 @@ export const command: Command = {
         const flag = await yargs(args.slice(0)).array(["settings", "value"]).argv;
         let embed: RichEmbed = new RichEmbed()
             .setColor(client.config.COLOR);
-        
+
         // Ignore for non-admin users
         if (message.author.id !== message.member.guild.ownerID && !message.member.permissions.has("manageGuild")) {
-                embed.setDescription(guildLanguage.ADMIN.CONFIG.NO_PERMS);
+            embed.setDescription(guildLanguage.ADMIN.CONFIG.NO_PERMS);
 
-            return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id }});
+            return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
         }
 
         const langOpt: string[] = ["english"];
@@ -29,13 +29,13 @@ export const command: Command = {
         if (!flag.settings) {
             embed.setDescription(guildLanguage.ADMIN.CONFIG.NO_OPT.replace("{options}", configOpt.join("`, `")));
 
-            return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id }});
+            return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
         }
 
         if (!configOpt.includes(flag.settings[0] as string)) {
             embed.setDescription(guildLanguage.ADMIN.CONFIG.NO_OPT.replace("{options}", configOpt.join("`, `")));
 
-            return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id }});
+            return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
         }
 
         switch (flag.settings[0]) {
@@ -43,7 +43,7 @@ export const command: Command = {
                 if (!flag.value[0]) {
                     embed.setDescription(guildLanguage.ADMIN.CONFIG.NO_PREFIX);
 
-                    return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id }});
+                    return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
                 }
 
                 if ((flag.value[0] as string).length > 64) {
@@ -55,7 +55,32 @@ export const command: Command = {
                 embed.setDescription(guildLanguage.ADMIN.CONFIG.PREFIX_SUCCESS.replace("{prefix}", flag.value[0] as string));
 
                 await client.database.set(`Database.${message.guildID}.Prefix`, flag.value[0]);
-                message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id }});
+                message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
+                break;
+            case "language":
+                if (!flag.value[0]) {
+                    embed.setDescription(guildLanguage.ADMIN.CONFIG.NO_LANG.replace("{language}", "English"));
+
+                    return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
+                }
+
+                if (!langOpt.includes((flag.value[0] as string).toLowerCase())) {
+                    embed.setDescription(guildLanguage.ADMIN.CONFIG.INVALID_LANG.replace("{language}", "English"));
+
+                    return message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
+                }
+
+                const cfgLanguage: string = (flag.value[0] as string).toLowerCase();
+
+                embed.setDescription(guildLanguage.ADMIN.CONFIG.LANG_SUCCESS.replace("{language}", cfgLanguage.charAt(0).toUpperCase() + cfgLanguage.slice(1)))
+
+                message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
+                client.database.set(`Database.${message.guildID}.Language`, (flag.value[0] as string).toUpperCase());
+                break;
+            default:
+                embed.setDescription(guildLanguage.ADMIN.CONFIG.NO_OPT.replace("{options}", configOpt.join("`, `")));
+
+                message.channel.createMessage({ embeds: [embed], messageReference: { messageID: message.id } });
                 break;
         }
 
