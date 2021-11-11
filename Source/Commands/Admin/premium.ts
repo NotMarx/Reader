aee71c0c6992ae60d7c0958c0e385303c7b7e84d
Reinhardt <contact@reinhello.xyz>
2021-11-11 15:48:39 +0700

    Handle error

--- Source/Commands/Admin/premium.ts
+++ Source/Commands/Admin/premium.ts
@@ -10,7 +10,15 @@ export const command: Command = {
         const user = message.member.guild.members.find((member) => member.id === args[1]) || message.member.guild.members.find((member) => member.username === args[1]) || message.mentions[0]
         const userIsPremium: boolean = client.database.fetch(`Database.${user.id}.Premium`);
 
+        if (!args[0]) {
+            return message.channel.createMessage({ content: `Please choose the correct option: \`add\`, \`remove\``, messageReference: { messageID: message.id } });
+        }
+
         if (args[0] === "add") {
+            if (!user) {
+                return message.channel.createMessage({ content: `Cannot find that user, please try again!`, messageReference: { messageID: message.id } });
+            }
+
             if (userIsPremium) {
                 return message.channel.createMessage({ content: `**${user.username}#${user.discriminator}** is already a Premium User!`, messageReference: { messageID: message.id } });
             } else {
@@ -20,6 +28,10 @@ export const command: Command = {
         }
 
         if (args[0] === "remove") {
+            if (!user) {
+                return message.channel.createMessage({ content: `Cannot find that user, please try again!`, messageReference: { messageID: message.id } });
+            }
+
             if (!userIsPremium) {
                 return message.channel.createMessage({ content: `**${user.username}#${user.discriminator}** isn't a Premium User at first!`, messageReference: { messageID: message.id } });
             } else {
