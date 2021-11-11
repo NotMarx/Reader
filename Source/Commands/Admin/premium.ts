957d672a5b99ce0d0d98dc6312328f3d96a85c50
Reinhardt <notmarx.tech@gmail.com>
2021-11-11 15:42:28 +0700

    Add command to handle Premium

---
+++ Source/Commands/Admin/premium.ts
@@ -0,0 +1,31 @@
+import { Command } from "../../Interfaces";
+
+export const command: Command = {
+    name: "premium",
+    aliases: [],
+    description: "Configure Premium Stuff",
+    category: "Admin",
+    adminOnly: true,
+    run: async (client, message, args) => {
+        const user = message.member.guild.members.find((member) => member.id === args[1]) || message.member.guild.members.find((member) => member.username === args[1]) || message.mentions[0]
+        const userIsPremium: boolean = client.database.fetch(`Database.${user.id}.Premium`);
+
+        if (args[0] === "add") {
+            if (userIsPremium) {
+                return message.channel.createMessage({ content: `**${user.username}#${user.discriminator}** is already a Premium User!`, messageReference: { messageID: message.id } });
+            } else {
+                client.database.set(`Database.${user.id}.Premium`, true);
+                return message.channel.createMessage({ content: `**${user.username}#${user.discriminator}** (\`${user.id}\`) is now a Premium User!`, messageReference: { messageID: message.id } });
+            }
+        }
+
+        if (args[0] === "remove") {
+            if (!userIsPremium) {
+                return message.channel.createMessage({ content: `**${user.username}#${user.discriminator}** isn't a Premium User at first!`, messageReference: { messageID: message.id } });
+            } else {
+                client.database.set(`Database.${user.id}.Premium`, false);
+            return message.channel.createMessage({ content: `**${user.username}** (\`${user.id}\`) is now a Normal User!`, messageReference: { messageID: message.id } });
+            }
+        }
+    }
+}
