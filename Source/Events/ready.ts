f4aff2b1c3141edf22595c2320a14e1ca5435151
Reinhardt <contact@reinhello.xyz>
2021-11-25 21:55:04 +0700

    Presence are now updated per shard

--- Source/Events/ready.ts
+++ Source/Events/ready.ts
@@ -6,7 +6,6 @@ import Logger from "../Extensions/logger";
 export const event: Event = {
     name: "ready",
     run: async (client) => {
-        client.editStatus("dnd", { name: "Reading...", type: 0 });
         Logger.success(`DISCORD`, `${client.user.username} Has Connected!`);
     }
 }
--- Source/Events/shardPreReady.ts
+++ Source/Events/shardPreReady.ts
@@ -8,6 +8,7 @@ export const event: Event = {
     run: async (client, id?: number) => {
         const latency = client.shards.get(id).latency;
 
+        client.shards.get(id).editStatus("dnd", { name: "Reading...", type: 0 });
         Logger.log(`SHARD READY | ID: ${id}`, `Shard #${id+ 1} Successfully Connected In ${(((id + 1) / client.shards.size) * 100).toFixed(1)}% With Ping: ${latency}ms`, "#FFA500");
     }
 }
