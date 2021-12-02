12e2604b6237c646f41f9b84103a34652389dc75
Reinhardt <notmarx.tech@gmail.com>
2021-12-02 16:53:01 +0700

    Add comment line

--- Source/Events/guildAvailable.ts
+++ Source/Events/guildAvailable.ts
@@ -8,6 +8,7 @@ export const event: Event = {
     run: async (client, guild: Guild) => {
         const guildSettings = client.db.findOne({ GuildID: guild.id });
 
+        // Create a database if guild is first-time available
         if (!guildSettings) {
             client.db.create({
                 GuildID: guild.id,
