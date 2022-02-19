1da16cabae6e4408b3aa92d8ae06945457cb950b
Reinhardt <notmarx.tech@gmail.com>
2022-02-19 12:01:23 +0700

    Create a database for fresh joined guild

--- Source/Events/guildCreate.ts
+++ Source/Events/guildCreate.ts
@@ -8,5 +8,15 @@ export const event: Event = {
     name: "guildCreate",
     run: async (client, guild: Guild) => {
         Logger.system("JOINED GUILD", `Connected To Guild: ${guild.name} (${guild.id}) | Total Guilds: ${client.guilds.size}`);
+
+        const guildHasDB: boolean = await client.database.fetch(`Database.${guild.id}`);
+        
+        if (!guildHasDB) {
+            client.database.set(`Database.${guild.id}`, {
+                Language: "ENGLISH",
+                Prefix: "n;",
+                ReadState: "current"
+            });
+        }
     }
 }
--- Source/Events/ready.ts
+++ Source/Events/ready.ts
@@ -7,5 +7,19 @@ export const event: Event = {
     name: "ready",
     run: async (client) => {
         Logger.success(`DISCORD`, `${client.user.username} Has Connected!`);
+
+        const guilds = client.guilds.map((guild) => guild.id);
+
+        for (let i = 0; i < guilds.length; i++) {
+            const guildHasDB: boolean = await client.database.fetch(`Database.${guilds[i]}`);
+
+            if (!guildHasDB) {
+                client.database.set(`Database.${guilds[i]}`, {
+                    Language: "ENGLISH",
+                    Prefix: "n;",
+                    ReadState: "current"
+                });
+            }
+        }
     }
 }
