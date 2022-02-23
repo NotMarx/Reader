0ffab84cfa17e96a398510d29ac5880a3ec6e9c9
Reinhardt <notmarx.tech@gmail.com>
2022-02-23 09:44:22 +0700

    Allow admin to access all help commands

--- Source/Commands/General/help.ts
+++ Source/Commands/General/help.ts
@@ -26,9 +26,96 @@ export const command: Command = {
             const nsfwOnly: "`True`" | "`False`" = command.nsfwOnly ? "`True`" : "`False`";
             const usage: string = command.usage ? `\`${prefix}${command.usage}\`` : "No Usage"
 
+            if (command.nsfwOnly && !(message.channel as TextChannel).nsfw && client.config.ADMIN_ID.includes(message.author.id)) {
+                const helpEmbed: EmbedOptions = {
+                    title: `${prefix}${name}`,
+                    description: description,
+                    fields: [
+                        {
+                            name: "Aliases",
+                            value: aliases,
+                            inline: true,
+                        },
+                        {
+                            name: "Category",
+                            value: category,
+                            inline: true
+                        },
+                        {
+                            name: "\u200b",
+                            value: "\u200b",
+                            inline: false
+                        },
+                        {
+                            name: "Usage",
+                            value: usage,
+                            inline: true
+                        },
+                        {
+                            name: "\u200b",
+                            value: "\u200b",
+                            inline: false
+                        },
+                        {
+                            name: "Admin Only",
+                            value: adminOnly,
+                            inline: true
+                        },
+                        {
+                            name: "NSFW Only",
+                            value: nsfwOnly,
+                            inline: true
+                        }
+                    ],
+                    color: client.config.COLOR,
+                    thumbnail: {
+                        url: client.user.avatarURL
+                    },
+                    timestamp: new Date().toISOString(),
+                    footer: {
+                        text: "Made by reinhardt"
+                    }
+                };
+
+                return message.channel.createMessage({ embeds: [helpEmbed], messageReference: { messageID: message.id } });
+
+                // If there is no args provided
+            } else {
+                const commands: Command[] = await client.commands.filter((cmd) => cmd.name !== "eval" && cmd.name !== "sweep");
+
+                let embed: RichEmbed = new RichEmbed()
+                    .setTitle(`${client.user.username}'s Commands`)
+                    .setDescription(`Use \`${prefix}help <command_name>\` for a command detail`)
+                    .setColor(client.config.COLOR)
+                    .setThumbnail(client.user.avatarURL)
+                    .setTimestamp()
+                    .setFooter("Made by reinhardt");
+
+                let com: object = {};
+
+                for (let comm of commands.values()) {
+                    const category = comm.category || "Unknown";
+                    const name = comm.name;
+
+                    if (!com[category]) {
+                        com[category] = [];
+                    }
+                    com[category].push(name);
+                }
+
+                for (const [key, value] of Object.entries(com)) {
+                    let category: string = key;
+                    let desc: string = "`" + (value as string[]).join("`, `") + "`";
+
+                    embed.addField(`${category} [${(value as string[]).length}]`, desc);
+                }
+
+                message.channel.createMessage({ embed: embed, messageReference: { messageID: message.id } });
+            }
+
             if (command.nsfwOnly && !(message.channel as TextChannel).nsfw) {
                 return message.channel.createMessage({ embeds: [{ description: "This command is only executable in **NSFW Channels**!", color: client.config.COLOR }], messageReference: { messageID: message.id } });
-            } 
+            }
 
             const helpEmbed: EmbedOptions = {
                 title: `${prefix}${name}`,
