d6772b9f30afbd2d3f73b74d5b1a8f47fde89e1a
Reinhardt <contact@reinhello.tech>
2022-07-12 11:19:22 +0700

    Add framework Event modules

---
+++ framework/lib/Events/Event.ts
@@ -0,0 +1,75 @@
+import { ReaderClient } from "../Client";
+import * as EventModules from "./Modules";
+
+export class ReaderEvent {
+
+    /**
+     * Reader client
+     */
+    private client: ReaderClient;
+
+    /**
+     * First paramter passed in the event
+     */
+    private firstParam?: any;
+
+    /**
+     * Second paramter passed in the event
+     */
+    private secondParam?: any;
+
+    /**
+     * Third paramter passed in the event
+     */
+    private thirdParam?: any;
+
+    /**
+     * Fourth paramter passed in the event
+     */
+    private fourthParam?: any;
+
+    /**
+     * Reader's Gateway event class
+     * @param client Reader client
+     * @param args Parameters passed for gateway events
+     */
+    constructor(client: ReaderClient, firstParam?: any, secondParam?: any, thirdParam?: any, fourthParam?: any) {
+        this.client = client;
+        this.firstParam = firstParam;
+        this.secondParam = secondParam;
+        this.thirdParam = thirdParam;
+        this.fourthParam = fourthParam;
+    }
+
+    /**
+     * Fires a `error` event
+     * @returns {void}
+     */
+    public errorEvent(): void {
+        return EventModules.errorEvent(this.client, this.firstParam, this.secondParam);
+    }
+
+    /**
+     * Fires a `interactionCreate` event
+     * @returns {Promise<void>}
+     */
+    public interactionCreateEvent(): Promise<void> {
+        return EventModules.interactionCreateEvent(this.client, this.firstParam);
+    }
+
+    /**
+     * Fires a `ready` event
+     * @returns {void}
+     */
+    public readyEvent(): void {
+        return EventModules.readyEvent(this.client);
+    }
+
+    /**
+     * Handle TypeScript unhandled rejection error
+     * @returns {void}
+     */
+    public tsError(): void {
+        return EventModules.tsError(this.firstParam);
+    }
+}
