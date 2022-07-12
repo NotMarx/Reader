45591dbbe3f1e7ec6b16ceaf2d616c35ae54ddde
Reinhardt <contact@reinhello.xyz>
2022-07-12 17:44:44 +0700

    Add `search` slash command

---
+++ bot/src/Commands/Main/Search.ts
@@ -0,0 +1,25 @@
+import { ReaderCommand, ReaderInterface } from "reader-framework";
+
+export const command: ReaderInterface.ICommand = {
+    name: "search",
+    description: "Search for NHentai doujin",
+    options: [
+        {
+            name: "query",
+            description: "The title query of the doujin",
+            type: 3,
+            channel_types: 0,
+            required: true
+        },
+        {
+            name: "page",
+            description: "Page of the results",
+            type: 4,
+            channel_types: 0
+        }
+    ],
+    type: 1,
+    run: (payload) => {
+        return new ReaderCommand(payload).searchCommand();
+    }
+}
