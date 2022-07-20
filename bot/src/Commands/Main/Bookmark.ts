8a96aa7497ea1f9496770e9c9436ea8bf14fcebb
Reinhardt <contact@reinhello.xyz>
2022-07-20 08:48:29 +0700

    Add `bookmark` slash command

---
+++ bot/src/Commands/Main/Bookmark.ts
@@ -0,0 +1,20 @@
+import { ReaderCommand, ReaderInterface } from "reader-framework";
+
+export const command: ReaderInterface.ICommand = {
+    name: "bookmark",
+    description: "Check the user bookmark library",
+    type: 1,
+    options: [
+        {
+            name: "user",
+            description: "The user to check",
+            type: 6,
+            required: false,
+            /* @ts-ignore */
+            channel_types: 0
+        }
+    ],
+    run: async (payload) => {
+        return new ReaderCommand(payload).bookmarkCommand();
+    }
+}
