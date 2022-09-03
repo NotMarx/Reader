b70d7d48ce336d22eb80ee22e8eda0b3d384882e
Reinhardt <contact@reinhello.tech>
2022-09-03 13:59:30 +0700

    Update botlist stats

--- framework/lib/Events/Modules/ReadyEvent.ts
+++ framework/lib/Events/Modules/ReadyEvent.ts
@@ -2,6 +2,8 @@ import { NReaderClient } from "../../Client";
 
 export function readyEvent(client: NReaderClient) {
     if (client.ready) {
+        client.apiStats.postStats("bhbotlist.tech", client.config.LIST.BHBOTLIST.AUTH);
+
         client.logger.info({ message: `${client.user.username}#${client.user.discriminator} Is Online`, subTitle: "NReaderFramework::Events::Ready", title: "GATEWAY" });
     }
 }
