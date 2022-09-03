edffd389f7ac49af2cafc95936812611caf79cf0
Reinhardt <contact@reinhello.tech>
2022-09-03 13:58:18 +0700

    Add new constant for botlist

--- framework/lib/Constant.ts
+++ framework/lib/Constant.ts
@@ -1,3 +1,10 @@
+const BHBotList = {
+    API: "https://api.bhbotlist.tech",
+    BASE: "https://bhbotlist.tech",
+    STATS: () => `${BHBotList.API}/bots/stats`,
+    VOTE: (id: string) => `${BHBotList.BASE}/bot/${id}/vote`,
+};
+
 const Source = {
     BASE: "https://nhentai.net",
     ID: (id: string) => `https://nhentai.net/g/${id}`,
@@ -5,5 +12,6 @@ const Source = {
 };
 
 export const NReaderConstant = {
+    BHBotList,
     Source
 };
