245931702607109399664efee86e0e4c631f6781
Reinhardt <contact@reinhello.tech>
2022-09-03 14:13:15 +0700

    Post topgg stats

--- framework/lib/Utils/APIStats.ts
+++ framework/lib/Utils/APIStats.ts
@@ -25,6 +25,18 @@ export class APIStats {
 
                 break;
             case "top.gg":
+                fetch(NReaderConstant.TopGG.STATS(this.client.user.id), {
+                    body: JSON.stringify({
+                        server_count: this.client.guilds.size,
+                        shard_count: this.client.shards.size
+                    }),
+                    headers: {
+                        "Authorization": authToken,
+                        "Content-Type": "application/json"
+                    },
+                    method: "POST"
+                });
+
                 break;
             default:
                 this.client.logger.error({ message: "Invalid Bot List", subTitle: "NReaderFramework::APIStats::postStats", title: "ERROR" });
