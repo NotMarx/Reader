e2af9264dbc9ad559225d31dfce8e102201c06e1
Reinhardt <contact@reinhello.tech>
2022-09-12 19:29:20 +0700

    Import from lib

--- bot/src/NReader.ts
+++ bot/src/NReader.ts
@@ -1,4 +1,4 @@
-import { NReaderClient } from "nreader-framework";
+import { NReaderClient } from "nreader-framework/lib";
 import * as Config from "../../config/config.json";
 
 const client = new NReaderClient(Config.BOT.TOKEN, {
