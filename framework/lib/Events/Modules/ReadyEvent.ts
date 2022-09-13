8b80fad04ff2cf2746bdf92db5dc33aca08f2532
Reinhardt <contact@reinhello.tech>
2022-09-13 15:10:57 +0700

    Add option to post botlist stats or not

--- framework/lib/Events/Modules/ReadyEvent.ts
+++ framework/lib/Events/Modules/ReadyEvent.ts
@@ -2,8 +2,10 @@ import { NReaderClient } from "../../Client";
 
 export function readyEvent(client: NReaderClient) {
     if (client.ready) {
-        client.apiStats.postStats("bhbotlist.tech", client.config.LIST.BHBOTLIST.AUTH);
-        client.apiStats.postStats("top.gg", client.config.LIST.TOPGG.AUTH);
+        if (client.config.LIST.ENABLED) {
+            client.apiStats.postStats("bhbotlist.tech", client.config.LIST.BHBOTLIST.AUTH);
+            client.apiStats.postStats("top.gg", client.config.LIST.TOPGG.AUTH);
+        }
 
         client.logger.info({ message: `${client.user.username}#${client.user.discriminator} Is Online`, subTitle: "NReaderFramework::Events::Ready", title: "GATEWAY" });
     }
