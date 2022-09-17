6a83bdcadd282ee9246f6c07794a925e3f8bf68b
Reinhardt <contact@reinhello.xyz>
2022-09-17 13:12:13 +0700

    Fix interaction response

--- framework/lib/Commands/Modules/SearchSimilarCommand.ts
+++ framework/lib/Commands/Modules/SearchSimilarCommand.ts
@@ -18,7 +18,7 @@ export async function searchSimilarCommand(client: NReaderClient, interaction: C
             .setColor(client.config.BOT.COLOUR)
             .setDescription(client.translate("main.tags.restricted", { channel: "[#info](https://discord.com/channels/763678230976659466/1005030227174490214)", server: "https://discord.gg/b7AW2Zkcsw" }));
 
-        return interaction.createMessage({
+        return interaction.createFollowup({
             embeds: [embed.data],
             flags: Constants.MessageFlags.EPHEMERAL
         });
@@ -33,7 +33,7 @@ export async function searchSimilarCommand(client: NReaderClient, interaction: C
                 .setColor(client.config.BOT.COLOUR)
                 .setDescription(client.translate("main.search.empty"));
 
-            return interaction.createMessage({
+            return interaction.createFollowup({
                 embeds: [embed.data],
             });
         }
