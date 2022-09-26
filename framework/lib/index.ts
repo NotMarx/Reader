e185959c68e3d3e9296b82b422a92cf66bb7fadc
Reinhardt <contact@reinhello.xyz>
2022-09-26 20:00:08 +0700

    Export Oceanic from framework

--- framework/lib/index.ts
+++ framework/lib/index.ts
@@ -3,4 +3,5 @@ export * from "./Commands";
 export * from "./Constant";
 export * from "./Events";
 export * as NReaderInterface from "./Interfaces";
+export * as NReaderOceanic from "oceanic.js";
 export * as NReaderUtil from "./Utils";
