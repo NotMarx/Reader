5347e7e2f196d619ec4a62016dc2ddba744ce680
Reinhardt <contact@reinhello.xyz>
2023-01-08 16:11:05 +0700

    Disable cache for messages

--- bot/src/NReader.ts
+++ bot/src/NReader.ts
@@ -3,6 +3,9 @@ import * as Config from "../../config/config.json";
 
 const client = new NReaderClient({
     auth: Config.BOT.TOKEN,
+    collectionLimits: {
+        messages: 0
+    },
     defaultImageFormat: "png",
     gateway: {
         intents: ["GUILDS"],
