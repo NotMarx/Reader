e7743a47f8d2843147584f6a812d40ed7b6db38e
Reinhardt <contact@reinhello.tech>
2023-01-08 15:35:13 +0700

    Add `shardPreReady` event

---
+++ bot/src/Events/ShardPreReady.ts
@@ -0,0 +1,8 @@
+import { NReaderEvent, NReaderInterface } from "nreader-framework/lib";
+
+export const event: NReaderInterface.IEvent = {
+    name: "shardPreReady",
+    run: (client, id: number) => {
+        return new NReaderEvent(client, id).shardPreReadyEvent();
+    }
+}
